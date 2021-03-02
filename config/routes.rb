e07092c0e565e1a4651c376124bd5e798bd89c4b
kabe0852@gmail.com <ec2-user@ip-172-31-60-128.ec2.internal>
2021-03-02 11:53:55 +0000

    finish apply3

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :homes, only:[:home, :about]
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
-    resources :comments, only: [:create, :destroy]
   end
 end
